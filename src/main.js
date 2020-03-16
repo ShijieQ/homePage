1d2c9111a496fb965bde12af7d0bd5bfa8a01f31
ShijieQ <ShijieQ@outlook.com>
2020-03-16 20:27:28 +0800

    Add header in homepage.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// 根据网址的不同，返回不同的内容给用户
 import router from './router'
-
+// before: npm install fastclick --save
+import fastClick from 'fastclick'
+// 统一浏览器显示格式
+import '@/assets/styles/reset.css'
+// 解决1像素边框问题
+import '@/assets/styles/border.css'
+// 引入图标库
+import '@/assets/styles/iconfont.css'
+// 解决移动端300ms延迟问题
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+	el: '#app',
+	router: router,
+	components: {
+		App
+	},
+	template: '<App/>'
 })
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<div class="iconfont back-icon">&#xeb99;</div>
+		</div>
+		<div class="header-input">个人信息</div>
+		<div class="header-right">
+			<span class="iconfont more-icon">&#xeb98;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeHeader'
+}	
+</script>
+
+<style lang="stylus" scoped>
+	.header
+		display: flex
+		line-height: .86rem
+		background: #00bcd4
+		color: #fff
+		.header-left
+			width: .86rem
+			float: left
+			text-align: center
+			
+		.header-input
+			flex: 1
+			height: .62rem
+			line-height: .62rem
+			padding-left: .1rem
+			padding-right: .1rem
+			margin-top: .12rem
+			margin-bottom: .12rem
+			border-radius: .1rem
+			background: #fff
+			color: #ccc
+			text-align: center
+		.header-right
+			width: .86rem
+			float: right
+			text-align: center
+</style>
